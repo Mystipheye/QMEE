1600e15c6960e78e1a4bbcb7443b465993bc317b
Mystipheye <bhargavr@mcmaster.ca>
2021-01-25 13:57:45 -0500

    Commit sent from Rstudio, by Rajat Bhargava.

--- assignment1data/Bio_708_Assignment_1.R
+++ assignment1data/Bio_708_Assignment_1.R
@@ -1,4 +1,3 @@
-setwd("QMEE")
 dir.create("assignment1data")
 library(tidyverse)
 readdata<-read_csv("./assignment1data/Mating_Data_Day_1.csv")
@@ -10,3 +9,6 @@ names(readdata)[names(readdata) == "mating duration"] <- "mating_duration"
 colnames(readdata)
 model<-lm(mating_duration~CODE,data=readdata)
 anova(model)
+
+
+
